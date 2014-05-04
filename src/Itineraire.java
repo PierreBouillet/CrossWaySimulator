315f1b400f636e7e7c00eac19c8e4557e089aa00
Audric Chabert <achabert@polytech.unice.fr>
2014-05-04 22:01:25 +0200

    ajout de la classe itineraire

---
+++ src/Itineraire.java
@@ -0,0 +1,17 @@
+/**
+ * Created by achabert on 04/05/2014.
+ */
+public enum Itineraire {
+    Nord_Sud,
+    Nord_Est,
+    Nord_Ouest,
+    Est_Nord,
+    Est_Sud,
+    Est_Ouest,
+    Ouest_Nord,
+    Ouest_Sud,
+    Ouest_Est,
+    Sud_Nord,
+    Sud_Est,
+    Sud_Ouest;
+}
